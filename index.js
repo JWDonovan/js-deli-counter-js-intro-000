29737d27287d936b191f888b6f385fd3e91ef2b0
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 18:23:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(array, person) {
   array.push(person);
   return `Welcome, ${person}. You are number ${array.length} in line.`;
 }
+
+function nowServing(array) {
+  if (array.length === 0) {
+    return 'There is nobody waiting to be served!';
+  } else {
+    let person = array.shift();
+    return `Currently serving ${array[array.length]}`;
+  }
+}
