a3bc7c9907a5aad3903578c425728331a8410c18
James Donovan <jameswilliamdonovan@gmail.com>
2020-10-18 18:31:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,18 @@ function nowServing(array) {
     return `Currently serving ${person}.`;
   }
 }
+
+function currentLine(line) {
+  if (line.length === 0) {
+    return 'The line is currently empty.';
+  } else {
+    let string = 'The line is currently: '
+    for (let i = 0; i < line.length; i++) {
+      string += `${i}. ${line[i]}`;
+
+      if (i !== line.length - 1) {
+        string += ', ';
+      }
+    }
+  }
+}
